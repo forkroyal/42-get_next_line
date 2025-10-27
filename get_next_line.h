f213c20eec6ecadefa99d3b6afc31f3f853fcfcc
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-27 20:41:29 +0100

    '600'

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 09:59:15 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/27 20:40:18 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/27 20:41:24 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,11 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-
 char	*read_line(int fd, char *text);
 char	*join_and_free(char *text, char *buffer);
 char	*get_line(char *text);
